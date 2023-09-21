cd7e5aeed9b821c29a6806d1513cb3913a40d364
Stuart Todd <stuarttodd444@gmail.com>
2023-09-21 13:49:21 +0100

    route fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Ecce\LaravelWeather\Controllers\WeatherController;
+use Ecce\LaravelWeather\Http\Controllers\WeatherController;
 
 Route::get('/weather-forecasts', [WeatherController::class, 'index'])->name('weather.index');
